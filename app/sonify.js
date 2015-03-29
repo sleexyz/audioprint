5a0e00ff9c4e35a2771b08cf1993fb955653c66c
Sean Lee <freshdried@gmail.com>
2015-03-28 20:00:56 -0400

    changed sonification

--- app/sonify.js
+++ app/sonify.js
@@ -4,6 +4,11 @@ synth.toMaster();
 
 Tone.Transport.start();
 var map = [
+    "C3",
+    "D3",
+    "E3",
+    "G3",
+    "A3",
     "C4",
     "D4",
     "E4",
@@ -14,19 +19,14 @@ var map = [
     "E5",
     "G5",
     "A5",
-    "C6",
-    "D6",
-    "E6",
-    "G6",
-    "A6",
-    "C7"];
+    "C6"];
 
 var basic = function(hash) {
-    for (var i = 0; i< hash.length; i++) {
+    for (var i = 0; i< 16; i++) {
         let val = parseInt(hash[i], 16);
         Tone.Transport.setTimeout(function(time) {
-            synth.triggerAttackRelease(map[val], "128n", time);
-        }, i/100);
+            synth.triggerAttackRelease(map[val], "256n", time);
+        }, i/25);
     }
 }
 export default basic;
